c66fb508d57200c4e082a4fdfac591b13e05d038
MonsteParty23 <lorelyn.paga@gmail.com>
2025-06-03 12:09:59 +0800

    initial commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const key = document.querySelectorAll('.key');
+const keys = document.querySelectorAll('.key');
 
     function playSound(key){
     const audio = document.querySelector(`audio[data-key="${key.toUpperCase()}"]`);
     const keyElement = document.querySelector(`.key[data-key="${key.toUpperCase()}"]`);
+    if(keyElement){
+        animateKey(keyElement); // Animate the key when pressed
+    }
     if (!audio) return; // Stop the function if no audio is found
 
     keyElement.classList.add('active');
@@ -17,3 +20,32 @@ const key = document.querySelectorAll('.key');
 window.addEventListener('keydown', function(event){
     playSound(event.key);
 });
+
+keys.forEach(key => {
+    key.addEventListener('click', function(){
+        const keyAttribute = this.getAttribute('data-key');
+        playSound(keyAttribute);
+    });
+});
+
+// Adding touch support for mobile devices
+keys.forEach(key => {
+    key.addEventListener('touchstart', function(e){
+        e.preventDefault(); // Prevent default touch behavior
+        const keyAttribute = this.getAttribute('data-key');
+        playSound(keyAttribute);
+    });
+});
+
+function animateKey(key){
+    key.animate([
+        // Keyframes for the animation
+        { transform: 'scale(1)', backgroundColor: '#333'},
+        { transform: 'scale(1.1)', backgroundColor: '#ffc600' },
+        { transform: 'scale(1)', backgroundColor: '#333' }
+    ], {
+        // Timing options
+        duration: 100,
+        iterations: 1,
+    });
+}
